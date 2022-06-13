e7d58c7b1ea54a3faa6f0d9cd0f16fb8b61ae38f
Mike Wright <m.wright92@icloud.com>
2022-06-13 16:33:22 -0700

    WIP feed method, route to pets
    Co-authored-by: Emilio Gonzales <emiliog1184@gmail.com>
    Co-authored-by: Gabe Nielsen <gabenielsen7@gmail.com>
    Co-authored-by: William Pratt <willpratt78@gmail.com>
    Co-authored-by: Brady Diamond <Brdydmnd@gmail.com>

--- app/models/neopet.rb
+++ app/models/neopet.rb
@@ -1,28 +1,52 @@
 class Neopet < ApplicationRecord
   require 'rufus-scheduler'
   belongs_to :user
-  before_save(:define_stats, :get_img)
-  validates :name, presence: true
-  validates :name, length: { in: 5..20 }
+  before_save(:define_stats, :get_img, :feed)
+  validates :pet_name, presence: true
+  validates :pet_name, length: { in: 5..20 }
   
-  private
-    def define_stats
-      self.att_stat = (rand(1..5) + rand(0..10))
-      self.def_stat = (rand(1..5) + rand(0..10))
-      self.spd_stat = (rand(1..5) + rand(0..10))
-      self.hp_stat = (rand(1..5) + rand(0..10))
-    end
+  
+  
+  def define_stats
+    self.att_stat = (rand(1..5) + rand(0..10))
+    self.def_stat = (rand(1..5) + rand(0..10))
+    self.spd_stat = (rand(1..5) + rand(0..10))
+    self.hp_stat = (rand(1..5) + rand(0..10))
+  end
 
-    def get_img
-      
-    end
+  def get_img
+    self.pet_pic = "/app/assets/images/neopets/#{rand(1..11)}.gif"
+  end
 
-    def feed 
-      scheduler = Rufus::Scheduler.new
-      self.pet_hungry = false
-      self.current_hp_stat = self.hp_stat
-      scheduler.in '1m' do
-        self.pet_hungry = true
+  def feed 
+    scheduler = Rufus::Scheduler.new
+    if self.pet_hungry == false
+      p "You put out the food, but it's not hungry yet"
+      self.waiting_to_feed? = true
+    else
+      self.eat
       end
     end
+  end
+
+  def waiting_to_feed?
+    if self.pet_hungry == false
+      return true 
+    else
+      self.eat
+    end
+  end
+end
+
+def eat
+  p "nom nom nom"
+      self.pet_hungry = false
+      self.current_hp_stat = self.hp_stat
+      self.pet_pic = "/app/assets/images/neopets/#{self.pet_pic.delete("^0-9")}.gif"
+      scheduler.in '10s', overlap: false do
+        p "feed me daddy"                                     
+        self.pet_pic = "/app/assets/images/neopets_sad/#{self.pet_pic.delete("^0-9")}.gif"
+        if self.waiting_to_feed?
+          self.eat
+        end
 end
